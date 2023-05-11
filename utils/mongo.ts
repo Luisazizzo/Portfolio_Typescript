5ff4a288a44a11a7e4f7f96c3c2be2948b12209b
Luisazizzo <luisa0zizzo@gmail.com>
2023-05-11 12:00:03 +0200

    chiusura Db

--- utils/mongo.ts
+++ utils/mongo.ts
@@ -1,18 +1,18 @@
-import { MongoClient, Db } from "mongodb";
+import { MongoClient } from "mongodb";
 
 const dbUrl = `mongodb+srv://luisaAdmin:${process.env
   .MONGODB_PASS!}@portfolio.hnogoqe.mongodb.net`;
 
-async function connectToDatabase(): Promise<Db> {
-  const client = await MongoClient.connect(dbUrl);
-  return client.db("portfolio");
-}
-
 export async function getDataFromDatabase(
   collectionName: string
 ): Promise<any[]> {
-  const db = await connectToDatabase();
-  const data = await db.collection(collectionName).find({}).toArray();
+  const client = await MongoClient.connect(dbUrl);
+  const data = await client
+    .db("portfolio")
+    .collection(collectionName)
+    .find({})
+    .toArray();
+  await client.close();
 
   return data;
 }
